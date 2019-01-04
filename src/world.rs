dc32293b2cc70a44dffbe8cb56281d5f9f3160a4
Sean Humeniuk <seanhumeniuk@gmail.com>
2019-01-03 23:11:47 -0500

    Move world object loading into method

--- src/world.rs
+++ src/world.rs
@@ -12,7 +12,6 @@ use noise::Perlin;
 use rect::{XYRect, XZRect, YZRect};
 use cube::Cube;
 use transform::{Translate, RotateY};
-use volume::ConstantMedium;
 use std::sync::Arc;
 
 pub struct World {
@@ -20,12 +19,6 @@ pub struct World {
 }
 
 impl World {
-    pub fn new() -> Self {
-        World {
-            hitables: Vec::new(),
-        }
-    }
-
     pub fn from_toml(scene: &Value) -> Self {
         let mut hitables: Vec<Box<Hitable>> = Vec::new();
 
@@ -36,102 +29,8 @@ impl World {
             let material_data = &scene["materials"].as_table().unwrap()[material_name];
             let material = World::create_material_from_toml(material_data, &scene["textures"]);
 
-            if obj_type == "sphere" {
-                let position = obj_data["position"].as_array().unwrap();
-                let x = position[0].as_float().unwrap() as f32;
-                let y = position[1].as_float().unwrap() as f32;
-                let z = position[2].as_float().unwrap() as f32;
-                let radius = obj_data["radius"].as_float().unwrap() as f32;
-
-                let sphere = Sphere::new(Vector3::new(x, y, z), radius, material);
-                hitables.push(Box::new(sphere));
-            } else if obj_type == "xyrect" {
-                let bounds = obj_data["bounds"].as_array().unwrap();
-                let x0 = bounds[0].as_float().unwrap() as f32;
-                let x1 = bounds[1].as_float().unwrap() as f32;
-                let y0 = bounds[2].as_float().unwrap() as f32;
-                let y1 = bounds[3].as_float().unwrap() as f32;
-                let k = obj_data["k"].as_float().unwrap() as f32;
-
-                let rect = XYRect::new(x0, x1, y0, y1, k, material);
-                let flip = obj_data["flip"].as_bool().unwrap_or(false);
-                if flip {
-                    let flipped = FlipNormals::new(Box::new(rect));
-                    hitables.push(Box::new(flipped));
-                } else {
-                    hitables.push(Box::new(rect));
-                }
-            } else if obj_type == "xzrect" {
-                let bounds = obj_data["bounds"].as_array().unwrap();
-                let x0 = bounds[0].as_float().unwrap() as f32;
-                let x1 = bounds[1].as_float().unwrap() as f32;
-                let z0 = bounds[2].as_float().unwrap() as f32;
-                let z1 = bounds[3].as_float().unwrap() as f32;
-                let k = obj_data["k"].as_float().unwrap() as f32;
-
-                let rect = XZRect::new(x0, x1, z0, z1, k, material);
-                
-                let flip = obj_data["flip"].as_bool().unwrap_or(false);
-                if flip {
-                    let flipped = FlipNormals::new(Box::new(rect));
-                    hitables.push(Box::new(flipped));
-                } else {
-                    hitables.push(Box::new(rect));
-                }
-            } else if obj_type == "yzrect" {
-                let bounds = obj_data["bounds"].as_array().unwrap();
-                let y0 = bounds[0].as_float().unwrap() as f32;
-                let y1 = bounds[1].as_float().unwrap() as f32;
-                let z0 = bounds[2].as_float().unwrap() as f32;
-                let z1 = bounds[3].as_float().unwrap() as f32;
-                let k = obj_data["k"].as_float().unwrap() as f32;
-
-                let rect = YZRect::new(y0, y1, z0, z1, k, material);
-
-                let flip = obj_data["flip"].as_bool().unwrap_or(false);
-                if flip {
-                    let flipped = FlipNormals::new(Box::new(rect));
-                    hitables.push(Box::new(flipped));
-                } else {
-                    hitables.push(Box::new(rect));
-                }       
-            } else if obj_type == "cube" {
-                let min = obj_data["min"].as_array().unwrap();
-                let x = min[0].as_float().unwrap() as f32;
-                let y = min[1].as_float().unwrap() as f32;
-                let z = min[2].as_float().unwrap() as f32;
-                let min = Vector3::new(x, y, z);
-
-                let max = obj_data["max"].as_array().unwrap();
-                let x = max[0].as_float().unwrap() as f32;
-                let y = max[1].as_float().unwrap() as f32;
-                let z = max[2].as_float().unwrap() as f32;
-                let max = Vector3::new(x, y, z);
-
-                let cube = Box::new(Cube::new(min, max, material));
-
-                let cube: Box<Hitable> = match obj_data.get("rotate_y") {
-                    Some(rotate_y) => {
-                        let y = rotate_y.as_float().unwrap() as f32;
-                        Box::new(RotateY::new(cube, y))
-                    },
-                    None => cube
-                };
-
-                let cube: Box<Hitable> = match obj_data.get("translate") {
-                    Some(translate) => {
-                        let translate = translate.as_array().unwrap();
-                        let x = translate[0].as_float().unwrap() as f32;
-                        let y = translate[1].as_float().unwrap() as f32;
-                        let z = translate[2].as_float().unwrap() as f32;
-
-                        Box::new(Translate::new(cube, Vector3::new(x, y, z)))
-                    },
-                    None => cube
-                };
-                
-                hitables.push(cube);
-            }
+            let hitable = World::create_object_from_toml(obj_type, obj_data, material);
+            hitables.push(hitable);
         }
 
         World {
@@ -186,8 +85,113 @@ impl World {
         }
     }
 
-    pub fn add_hitable(&mut self, hitable: Box<Hitable>) {
-        self.hitables.push(hitable);
+    fn create_object_from_toml(obj_type: &str, obj_data: &Value, material: Arc<Material>) -> Box<Hitable> {
+        if obj_type == "sphere" {
+            let position = obj_data["position"].as_array().unwrap();
+            let x = position[0].as_float().unwrap() as f32;
+            let y = position[1].as_float().unwrap() as f32;
+            let z = position[2].as_float().unwrap() as f32;
+            let radius = obj_data["radius"].as_float().unwrap() as f32;
+
+            let sphere: Box<Hitable> = Box::new(Sphere::new(Vector3::new(x, y, z), radius, material));
+
+            sphere
+        } else if obj_type == "xyrect" {
+            let bounds = obj_data["bounds"].as_array().unwrap();
+            let x0 = bounds[0].as_float().unwrap() as f32;
+            let x1 = bounds[1].as_float().unwrap() as f32;
+            let y0 = bounds[2].as_float().unwrap() as f32;
+            let y1 = bounds[3].as_float().unwrap() as f32;
+            let k = obj_data["k"].as_float().unwrap() as f32;
+
+            let rect = XYRect::new(x0, x1, y0, y1, k, material);
+            let flip = obj_data["flip"].as_bool().unwrap_or(false);
+            if flip {
+                let flipped = FlipNormals::new(Box::new(rect));
+                Box::new(flipped)
+            } else {
+                let rect: Box<Hitable> = Box::new(rect);
+                rect
+            }
+        } else if obj_type == "xzrect" {
+            let bounds = obj_data["bounds"].as_array().unwrap();
+            let x0 = bounds[0].as_float().unwrap() as f32;
+            let x1 = bounds[1].as_float().unwrap() as f32;
+            let z0 = bounds[2].as_float().unwrap() as f32;
+            let z1 = bounds[3].as_float().unwrap() as f32;
+            let k = obj_data["k"].as_float().unwrap() as f32;
+
+            let rect = XZRect::new(x0, x1, z0, z1, k, material);
+            
+            let flip = obj_data["flip"].as_bool().unwrap_or(false);
+            if flip {
+                let flipped = FlipNormals::new(Box::new(rect));
+                Box::new(flipped)
+            } else {
+                let rect: Box<Hitable> = Box::new(rect);
+                rect
+            }
+        } else if obj_type == "yzrect" {
+            let bounds = obj_data["bounds"].as_array().unwrap();
+            let y0 = bounds[0].as_float().unwrap() as f32;
+            let y1 = bounds[1].as_float().unwrap() as f32;
+            let z0 = bounds[2].as_float().unwrap() as f32;
+            let z1 = bounds[3].as_float().unwrap() as f32;
+            let k = obj_data["k"].as_float().unwrap() as f32;
+
+            let rect = YZRect::new(y0, y1, z0, z1, k, material);
+
+            let flip = obj_data["flip"].as_bool().unwrap_or(false);
+            if flip {
+                let flipped = FlipNormals::new(Box::new(rect));
+                Box::new(flipped)
+            } else {
+                let rect: Box<Hitable> = Box::new(rect);
+                rect
+            }       
+        } else if obj_type == "cube" {
+            let min = obj_data["min"].as_array().unwrap();
+            let x = min[0].as_float().unwrap() as f32;
+            let y = min[1].as_float().unwrap() as f32;
+            let z = min[2].as_float().unwrap() as f32;
+            let min = Vector3::new(x, y, z);
+
+            let max = obj_data["max"].as_array().unwrap();
+            let x = max[0].as_float().unwrap() as f32;
+            let y = max[1].as_float().unwrap() as f32;
+            let z = max[2].as_float().unwrap() as f32;
+            let max = Vector3::new(x, y, z);
+
+            let cube = Box::new(Cube::new(min, max, material));
+
+            let cube: Box<Hitable> = match obj_data.get("rotate_y") {
+                Some(rotate_y) => {
+                    let y = rotate_y.as_float().unwrap() as f32;
+                    Box::new(RotateY::new(cube, y))
+                },
+                None => cube
+            };
+
+            let cube: Box<Hitable> = match obj_data.get("translate") {
+                Some(translate) => {
+                    let translate = translate.as_array().unwrap();
+                    let x = translate[0].as_float().unwrap() as f32;
+                    let y = translate[1].as_float().unwrap() as f32;
+                    let z = translate[2].as_float().unwrap() as f32;
+
+                    Box::new(Translate::new(cube, Vector3::new(x, y, z)))
+                },
+                None => cube
+            };
+            
+            cube
+        } else {
+            panic!("Unknown object type");
+        }
+    }
+
+    pub fn hitables(self) -> Vec<Box<Hitable>> {
+        self.hitables
     }
 }
 
